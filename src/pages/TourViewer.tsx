7c85b1107e43de1e90fb9530859d48181d2bd71a
Ahmed-bna <161530661+Ahmed-bna@users.noreply.github.com>
2026-04-11 16:48:32 +0100

    feat: enable Dollhouse and Floor Plan views in all tours

--- src/pages/TourViewer.tsx
+++ src/pages/TourViewer.tsx
@@ -299,7 +299,7 @@ const CURRENCY_SYMBOLS: Record<string, string> = { EUR: "€", USD: "$", TND: "T
 function buildEmbedUrl(tourUrl: string, withSdkKey = false): string {
   const modelId = extractModelId(tourUrl);
   if (!modelId) return tourUrl;
-  return `https://my.matterport.com/show/?m=${modelId}&play=1&qs=1&title=0&vr=0&dh=0&f=0&search=0&lang=fr&applicationKey=${SDK_KEY}`;
+  return `https://my.matterport.com/show/?m=${modelId}&play=1&qs=1&title=0&vr=0&dh=1&f=1&search=0&lang=fr&applicationKey=${SDK_KEY}`;
 }
 
 const TourViewer = () => {
